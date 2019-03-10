93bfd6d8a376b057dd10dc62d0ac362f5cc0f2b9
Anton Panov <hightemp.unknown@gmai.com>
2019-03-10 21:10:13 +0300

    update

--- src/task.c
+++ src/task.c
@@ -21,9 +21,121 @@
  * Здесь алгоритм решения такой же как и в задаче о 8 ферзях. Разница только в проверке положения коня.
  */
 
+int fnFoundPaths(char caField[], char caVisitedFields[], int iaLengths[], int iLengthsIndex, int iStartX, int iStartY, int iFinishX, int iFinishY)
+{
+	caVisitedFields[iStartY*5+iStartX] = 1;
+
+	int iLengthsOldIndex = iLengthsIndex;
+	int bFinishFound = 0;
+	int bChildrenFinishFound = 0;
+
+	printf("%d %d %d\n", iLengthsIndex, iStartX, iStartY, iStartY*5+iStartX-1);
+
+	if (iStartX-1>0) {
+		if (caVisitedFields[iStartY*5+iStartX-1] != 1) {
+			iLengthsIndex++;
+			iaLengths[iLengthsIndex]++;
+			if (iStartX-1==iFinishX && iStartY==iFinishY) {
+				bFinishFound = 1;
+			} else {
+				bChildrenFinishFound = fnFoundPaths(caField, caVisitedFields, iaLengths, iLengthsIndex, iStartX-1, iStartY, iFinishX, iFinishY);
+			}
+		}
+	}
+	if (iStartX+1<5) {
+		if (caVisitedFields[iStartY*5+iStartX+1] != 1) {
+			iLengthsIndex++;
+			iaLengths[iLengthsIndex]++;
+			if (iStartX+1==iFinishX && iStartY==iFinishY) {
+				bFinishFound = 1;
+			} else {
+				bChildrenFinishFound = fnFoundPaths(caField, caVisitedFields, iaLengths, iLengthsIndex, iStartX+1, iStartY, iFinishX, iFinishY);
+			}
+		}
+	}
+	if (iStartY-1>0) {
+		if (caVisitedFields[(iStartY-1)*5+iStartX] != 1) {
+			iLengthsIndex++;
+			iaLengths[iLengthsIndex]++;
+			if (iStartX==iFinishX && iStartY-1==iFinishY) {
+				bFinishFound = 1;
+			} else {
+				bChildrenFinishFound = fnFoundPaths(caField, caVisitedFields, iaLengths, iLengthsIndex, iStartX, iStartY-1, iFinishX, iFinishY);
+			}
+		}
+	}
+	if (iStartY+1<5) {
+		if (caVisitedFields[(iStartY+1)*5+iStartX] != 1) {
+			iLengthsIndex++;
+			iaLengths[iLengthsIndex]++;
+			if (iStartX==iFinishX && iStartY+1==iFinishY) {
+				bFinishFound = 1;
+			} else {
+				bChildrenFinishFound = fnFoundPaths(caField, caVisitedFields, iaLengths, iLengthsIndex, iStartX, iStartY+1, iFinishX, iFinishY);
+			}
+		}
+	}
+
+	caVisitedFields[iStartY*5+iStartX] = 0;
+
+	if (bFinishFound) {
+		return bFinishFound;
+	}
+
+	iaLengths[iLengthsOldIndex] = 0;
+
+	return bChildrenFinishFound;
+}
+
 void fnTask1()
 {
+	char caField[] = {
+		1, 1, 1, 1, 1,
+		1, 0, 1, 0, 1,
+		1, 1, 1, 1, 1,
+		1, 0, 1, 0, 1,
+		1, 1, 1, 1, 1
+	};
 
+	char caVisitedFields[25] = {0};
+
+	int iStartX = 0;
+	int iStartY = 0;
+
+	int iFinishX = 4;
+	int iFinishY = 4;
+
+	int iaLengths[100000] = {0};
+	int iLengthsIndex = 0;
+
+	printf("Start position X: %d\n", iStartX);
+	printf("Start position Y: %d\n", iStartY);
+	printf("End position X: %d\n", iFinishX);
+	printf("End position Y: %d\n", iFinishY);
+
+	printf("Field: \n");
+	for (int iIndexX=0; iIndexX<5; iIndexX++) {
+		for (int iIndexY=0; iIndexY<5; iIndexY++) {
+			printf("%d ", caField[iIndexY*5+iIndexX]);
+		}
+		printf("\n");
+	}
+
+	fnFoundPaths(caField, caVisitedFields, iaLengths, iLengthsIndex, iStartX, iStartY, iFinishX, iFinishY);
+
+	printf("\n");
+
+	int iCount = 0;
+
+	for (int iIndex=0; iIndex<100000; iIndex++) {
+		//printf("%d ", iaLengths[iIndex]);
+		if (iaLengths[iIndex]>0) {
+			iCount++;
+		}
+	}
+	printf("\n");
+
+	printf("Found paths: %d\n", iCount);
 }
 
 char* maxLCS(char* caArray1, int iArray1Length, char* caArray2, int iArray2Length)
